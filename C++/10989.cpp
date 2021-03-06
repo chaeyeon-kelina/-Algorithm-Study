98cc89e5065c7e7198f29e917e44ec40017b1c5b
kkunmando <69005820+kkunmando@users.noreply.github.com>
2021-03-06 23:53:36 +0900

    C++ study : 사칙연산

---
+++ C++/10989.cpp
@@ -0,0 +1,19 @@
+//사칙연산_C++
+//10869 
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a, b;
+	cin >> a;
+	cin >> b;
+
+	cout << a + b << endl;
+	cout << a - b << endl;
+	cout << a * b << endl;
+	cout << a / b << endl;
+	cout << a % b << endl;
+
+	return 0;
+}
