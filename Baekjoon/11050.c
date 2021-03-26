327803cd2ddf38d038c2b6cd1d800b627cb3d9cb
Kim ChaeYeon <69005820+chaeyeon-kelina@users.noreply.github.com>
2021-03-26 23:46:17 +0900

    Create 11050.c

---
+++ Baekjoon/11050.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+int Factorial(int a) {
+	int result = 1;
+	for (int i = 1; i <= a; i++) {
+		result *= i;
+	}
+	return result;
+}
+
+int main() {
+	int N, K;
+
+	scanf("%d", &N);
+	scanf("%d", &K);
+
+	int a = Factorial(N);
+	int b = Factorial(K);
+	int c = Factorial(N - K);
+	int answer = a / (b*c);
+
+	printf("%d", answer);
+
+	return 0;
+}
