bd8c19f30ecaab9af9a3a04092018b901d5c3701
chaeyeon <321codus@naver.com>
2023-01-18 18:14:02 +0900

    기본문제 풀이

---
+++ basic-algo-lecture/0x03/1475.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool desc(float a, float b){
+    return a > b;
+}
+
+int main(void){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    float arr[10] = {0, };  //0~8 배열
+    int n;  //다솜이 방 번호
+    cin >> n;   
+    
+    while(n > 0){
+        int tmp = n%10;
+        if(tmp == 6 || tmp == 9)
+            arr[6] += 0.5;
+        else
+            arr[tmp]++;
+        n /= 10;
+    }
+
+    sort(arr, arr+10, desc);       //내림차순
+    int max = arr[0]+0.5;       //반올림
+    cout << max;
+    
+}
