92cfa61b6e79a4e1e62ad345775a77e7c29a6df4
chaeyeon <321codus@naver.com>
2023-01-22 23:11:43 +0900

    기본문제 3 풀다맘

--- basic-algo-lecture/0x03/11328.cpp
+++ basic-algo-lecture/0x03/11328.cpp
@@ -1,8 +1,45 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(void){
+int main(void)
+{
     ios::sync_with_stdio(0);
     cin.tie(0);
 
+    int n;
+    cin >> n;
+    bool same[1001];
+    int k = 0;
+    for (k = 0; k < n; k++)
+    {
+        int ans = 1;
+        fill(same, same + 1001, false);
+        string str1 = "";
+        string str2 = "";
+        cin >> str1 >> str2;
+        for (int i = 0; i < str1.size(); i++)
+        {
+            for (int j = 0; j < str2.size(); j++)
+            {
+                if (str1[i] == str2[j] && same[i] == false)
+                {
+                    same[i] = true;
+                    continue;
+                }
+            }
+        }
+        for (int i = 0; i < str2.size(); i++)
+        {
+            if (!same[i])
+            {
+                cout << "Impossible\n";
+                ans = 0;
+                continue;
+            }
+        }
+        if (ans == 1)
+        {
+            cout << "Possible\n";
+        }
+    }
 }
