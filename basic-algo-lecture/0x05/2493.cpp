ac397266211c93b8133c933a8edcc81bf034ffbf
chaeyeon <321codus@naver.com>
2023-01-31 23:55:15 +0900

    응용문제 2 풀이

---
+++ basic-algo-lecture/0x05/2493.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int n;
+    cin >> n;
+    stack<int> s;
+    for(int i=0; i<n; i++){
+        int t;
+        cin >> t;
+        s.push(t);
+    }
+    
+}
