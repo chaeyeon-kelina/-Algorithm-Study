310537503517cad8afa443ed0350f835c195ad8b
chaeyeon <321codus@naver.com>
2023-02-04 01:21:49 +0900

    응용문제 2 풀이

--- basic-algo-lecture/0x05/2493.cpp
+++ basic-algo-lecture/0x05/2493.cpp
@@ -5,29 +5,20 @@ int main(void){
     ios::sync_with_stdio(0);
     cin.tie(0);
 
+    stack<pair<int, int>> s;        //height, num
+
     int n;
     cin >> n;
-    stack<int> s;
-    for(int i=0; i<n; i++){
-        int t;
-        cin >> t;
-        s.push(t);
-    }
-    stack<int> s2;
-    while(n--){
-        int num;
-        num = s.top();
-        s.pop();
-        if(s.top() < num){
-            s2.push(num);
-            num = s.top();
+    
+    s.push({100000001, 0});
+    for(int i=1; i<=n; i++){
+        int h;
+        cin >> h;
+        while(s.top().first < h){
             s.pop();
         }
-        if(s2.top() < num){
-            
-        }
+        cout << s.top().second << " ";
+        s.push({h, i});
     }
-
-
 
 }
